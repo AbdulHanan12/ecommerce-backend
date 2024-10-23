6135b9a1dd006e818a8c9f1641ae71179678416b
Abdul Hanan <hananbhatti440@gmail.com>
2024-10-23 23:16:14 +0500

    Fix Validation Status Cide

--- validations/updatePasswordValidation.js
+++ validations/updatePasswordValidation.js
@@ -26,7 +26,7 @@ const validateUserPassword = [
 const handleValidationErrors = (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({
+      return res.status(422).json({
         success: false,
         errors: errors.array().map(error => ({
           field: error.param,
